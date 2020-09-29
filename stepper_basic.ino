c7c4c5a189e38226ab129a6dc9910b4f7575ffd3
Glenn Salaman <g.salaman@comcast.net>
2020-09-29 10:21:49 -0600

    Update stepper_basic.ino

--- stepper_basic.ino
+++ stepper_basic.ino
@@ -1,10 +1,12 @@
+/* Big Easy Driver version.  More current, 16th step control */
 
-
-#define STEP_PIN  2
-#define DIR_PIN   3
-#define MS1_PIN   4
-#define MS2_PIN   5  
+#define STEP_PIN   2
+#define DIR_PIN    3
+#define MS1_PIN    4
+#define MS2_PIN    5  
 #define ENABLE_PIN 6
+#define MS3_PIN    7  // should really put this next to MS1 and MS2, but I want to make it just
+                      // an addition to the previous "easy driver".
 
 void setup() 
 {
@@ -14,17 +16,25 @@ void setup()
   pinMode(DIR_PIN, OUTPUT);
   pinMode(MS1_PIN, OUTPUT);
   pinMode(MS2_PIN, OUTPUT);
+  pinMode(MS3_PIN, OUTPUT);
   pinMode(ENABLE_PIN, OUTPUT);
 
-  //set step size.
+  digitalWrite(STEP_PIN, LOW);
   digitalWrite(ENABLE_PIN, LOW);
 
   digitalWrite(DIR_PIN, HIGH);
 
-  // LOW/LOW = full step.
-  // HIGH/HIGH = 1/8 step
-  digitalWrite(MS1_PIN, HIGH);
-  digitalWrite(MS2_PIN, HIGH);
+  /* Truth table for MS selects:
+   *  MS1  MS2  MS3   Step 
+   *  L    L    L     Full
+   *  H    L    L     Half
+   *  L    H    L     Quarter
+   *  H    H    L     Eighth
+   *  H    H    H     Sixteenth
+   */
+  digitalWrite(MS1_PIN, LOW);
+  digitalWrite(MS2_PIN, LOW);
+  digitalWrite(MS3_PIN, LOW);
 
   Serial.println("inited");
 }
