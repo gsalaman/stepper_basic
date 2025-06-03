1082e8a672649a7280d4c864f8bb24f73b7818fc
Glenn Salaman <g.salaman@comcast.net>
2025-06-03 16:22:59 -0600

    Update stepper_basic.ino

--- stepper_basic.ino
+++ stepper_basic.ino
@@ -6,6 +6,10 @@
 #define MS2_PIN   5  
 #define ENABLE_PIN 6
 
+#define STEPS_PER_ROTATION 540
+int step_count = 0;
+int dir=LOW;
+
 void setup() 
 {
   Serial.begin(9600);
@@ -18,7 +22,7 @@ void setup()
   
   digitalWrite(ENABLE_PIN, LOW);
 
-  digitalWrite(DIR_PIN, HIGH);
+  digitalWrite(DIR_PIN, dir);
 
   // LOW/LOW = full step.
   // HIGH/HIGH = 1/8 step
@@ -31,10 +35,31 @@ void setup()
 void loop() 
 {
 
-    digitalWrite(STEP_PIN, HIGH);
-    delay(1);
-    digitalWrite(STEP_PIN,LOW);
-    delay(1);
+  // According to the data sheet, we've got 516.096 steps per revolution.
+  // We're gonna start by trying one full rotation fwd, then one back (then repeat)
 
+  // Start by taking a step.
+  digitalWrite(STEP_PIN, HIGH);
+  delay(1);
+  digitalWrite(STEP_PIN,LOW);
+  delay(2);
+  step_count++;
+
+  if (step_count >= STEPS_PER_ROTATION)
+  {
+    // set the new direction
+    if (dir == HIGH)
+    {
+      dir = LOW;
+    }
+    else
+    {
+      dir = HIGH;
+    }
+    digitalWrite(DIR_PIN, dir);
+
+    // reset our count
+    step_count = 0;
+  }
 
 }
